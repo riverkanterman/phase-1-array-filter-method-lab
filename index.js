470939be9ccf6cb37ba69db070cf0bc6361e0471
River <+>
2022-04-26 22:45:47 -0400

    commit tree 7b3465a00859b7e88a3ab0d0c6434515ee22e02e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string) {
+    return array.filter(function(el) {
+        return el.toLowerCase().indexOf(string.toLowerCase())!== -1
+    })
+}
+
+  function fuzzyMatch(array, string){
+    return array.filter(function(el) {
+        return el.startsWith(string)
+    })
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+      return driver.name === name
+    })
+  }
